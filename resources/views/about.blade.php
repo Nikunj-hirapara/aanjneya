b6be29d19ce8a996e073b552b1d67f944104f851
Ashish Vekariya <ashish.vekariya2272@gmail.com>
2022-02-20 09:26:31 +0530

    about us changes

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,7 +1,7 @@
 @extends('layout')
 @section('content')
 
-<!-- Start Page Title Area -->
+		<!-- Start Page Title Area -->
         <div class="page-title-area item-bg-1 jarallax"  data-jarallax='{"speed": 0.3}'>
 			<div class="container">
 				<div class="page-title-content">
@@ -24,5 +24,79 @@
 			</div>
 		</div>
 		<!-- End Page Title Area -->
+
+		<!-- Start About Area -->
+		<section class="about-area about-area-two pt-100">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-lg-6">
+						<div class="about-content">
+							<span>Know About Us </span>
+							<h2>Reliable and express logistics and transport solutions That no waste your time!</h2>
+							<p class="mb-2">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi nam dolorem reiciendis dolor voluptatum eveniet vero incidunt odit? Tempore optio deleniti qui repellendus aspernatur ratione officia deserunt mollitia illum natus. Lorem, ipsum dolor sit amet.</p>
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui minima rerum sed fuga consequatur voluptas quod voluptates! Ipsum reprehenderit voluptates inventore blanditiis, dolores corporis</p>
+						</div>
+					</div>
+					<div class="col-lg-6 pr-0">
+						<div class="about-img inner-about-img">
+							<div class="about-list">
+								<h3>Our Prime priorities:</h3>
+								<ul>
+									<li>
+										<i class="flaticon-checked"></i>
+										Customer and employee solutions are time-consuming.
+									</li>
+									<li>
+										<i class="flaticon-checked"></i>
+										We accomplish our goals more efficiently.
+									</li>
+									<li>
+										<i class="flaticon-checked"></i>
+										Transparent Communication Collaboration properly do.
+									</li>
+									<li>
+										<i class="flaticon-checked"></i>
+										It provides a road map to increased productivity.
+									</li>
+									<li>
+										<i class="flaticon-checked"></i>
+										Providing independent advice for you.
+									</li>
+									<li>
+										<i class="flaticon-checked"></i>
+										24/7 support system available
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- End About Area -->
+
+		<!-- Start About Area -->
+		<section class="about-area ptb-100">
+			<div class="container">
+				<div class="row align-items-center">
+					<div class="col-lg-6">
+						<div class="about-img">
+							<img src="assets/img/about-img.jpg" alt="Image">
+							<div class="about-quatre">
+								<i class="bx bxs-quote-alt-left bx-tada"></i>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-6">
+						<div class="about-content ml-30">
+							<span>About Us</span>
+							<h2>Reliable and express logistics and transport solutions That no waste your time!</h2>
+							<p class="mb-0">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi nam dolorem reiciendis dolor voluptatum eveniet vero incidunt odit? Tempore optio deleniti qui repellendus aspernatur  nam dolorem reiciendis dolor voluptatum eveniet ratione officia deserunt mollitia illum natus. Lorem, ipsum dolor  deleniti qui repellendus aspernatur ratione sit amet.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- End About Area -->
 
 @stop
